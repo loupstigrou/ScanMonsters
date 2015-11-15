ce7a2e4729a7fb1a15ec1d677b871c4647bb90e9
MajorMaad <quentin.audinot@gmail.com>
2015-11-15 14:33:16 -0500

    Localisation de l'utilisateur (recuperation longitude, latitude, altitude et precision)

--- ScanMonsters/app/src/main/java/prog_mobile/uqac/com/scanmonsters/LocationActivity.java
+++ ScanMonsters/app/src/main/java/prog_mobile/uqac/com/scanmonsters/LocationActivity.java
@@ -1,16 +1,43 @@
 package prog_mobile.uqac.com.scanmonsters;
 
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.location.LocationProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
-public class LocationActivity extends AppCompatActivity {
+public class LocationActivity extends AppCompatActivity implements LocationListener {
+
+    private LocationManager lm;
+    private double latitude;
+    private double longitude;
+    private double altitude;
+    private float accuracy;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        lm = (LocationManager) this.getSystemService(LOCATION_SERVICE);
+
+        if (lm.isProviderEnabled(LocationManager.GPS_PROVIDER))
+            lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10000, 0, this);
+        lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 10000, 0, this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        lm.removeUpdates(this);
     }
 
     @Override
@@ -33,5 +60,53 @@ public class LocationActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        latitude = location.getLatitude();
+        longitude = location.getLongitude();
+        altitude = location.getAltitude();
+        accuracy = location.getAccuracy();
+
+        String msg = String.format(
+                getResources().getString(R.string.new_location), latitude,
+                longitude, altitude, accuracy);
+        Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        String newStatus = "";
+        switch (status) {
+            case LocationProvider.OUT_OF_SERVICE:
+                newStatus = "OUT_OF_SERVICE";
+                break;
+            case LocationProvider.TEMPORARILY_UNAVAILABLE:
+                newStatus = "TEMPORARILY_UNAVAILABLE";
+                break;
+            case LocationProvider.AVAILABLE:
+                newStatus = "AVAILABLE";
+                break;
+        }
+        String msg = String.format(
+                getResources().getString(R.string.provider_new_status), provider,
+                newStatus);
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        String msg = String.format(
+                getResources().getString(R.string.provider_disabled), provider);
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        String msg = String.format(
+                getResources().getString(R.string.provider_enabled), provider);
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
     }
 }
